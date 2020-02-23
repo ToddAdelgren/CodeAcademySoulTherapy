3f89dda2e525c6469ca865d845b36e8d9974202d
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-02-23 10:23:32 -0600

    Changes Before Uploading To S3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SplashComponent } from './components/splash/splash.component';
-import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { SplashMainComponent } from './components/splash-main/splash-main.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { PrivacyPolicyComponent } from './components/privacy-policy/privacy-policy.component';
 import { TermsOfuseComponent } from './components/terms-ofuse/terms-ofuse.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MaterialModule} from './material-module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     SplashComponent,
-    HeaderComponent,
     FooterComponent,
-    SplashMainComponent,
     LoginComponent,
     SignupComponent,
     PrivacyPolicyComponent,
@@ -26,7 +24,11 @@ import { TermsOfuseComponent } from './components/terms-ofuse/terms-ofuse.compon
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
+
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  signupForm: FormGroup;
+  hide: boolean;
+
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      emailAddress: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])]
+      });
   }
 
+  signup(){
+    console.log("signup()")
+    //if(this.signupForm.valid){
+      //console.log('DATA IS VALID');
+      //this.userStore.login(this.loginForm.value.username, this.loginForm.value.password);
+    //}
+  }
 }
