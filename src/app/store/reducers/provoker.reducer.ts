29bca61d614956f0ef38a5c907741598e216b72c
Todd Adelgren <toddadelgren@Todds-MacBook-Air.local>
2020-03-09 16:24:21 -0500

    Added File provoker.reducer.ts

---
+++ src/app/store/reducers/provoker.reducer.ts
@@ -0,0 +1,20 @@
+import * as provokerActions from '../actions/provoker.action'
+import { createReducer, on, Action } from '@ngrx/store';
+// import { createReducer, on, Action } from '@ngrx/store';
+
+export interface ProvokerState {
+    beingDisplayed: number
+};
+
+export const initialProvokerState: ProvokerState = {
+    beingDisplayed: 0
+};
+
+const provokerReducer = createReducer(
+    initialProvokerState,
+    on(provokerActions.setBeingDisplayed, (state, {id})=> ({...state, beingDisplayed: id})),
+);
+
+export function reducer(state: ProvokerState, action: Action){
+    return provokerReducer(state, action);
+}
